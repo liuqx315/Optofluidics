96ba54686f3438437eb3cb8029f86defbbb20137
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2014-10-12 21:23:26 +0200

    Compute and store track features related to velocity thresholding.
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src/main/java/com/optofluidics/trackmate/action/VelocityAnalysisAction.java
+++ src/main/java/com/optofluidics/trackmate/action/VelocityAnalysisAction.java
@@ -2,21 +2,29 @@ package com.optofluidics.trackmate.action;
 
 import java.awt.Frame;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.swing.ImageIcon;
 
+import org.jgrapht.graph.DefaultWeightedEdge;
 import org.scijava.plugin.Plugin;
 
 import fiji.plugin.trackmate.Dimension;
-import fiji.plugin.trackmate.Logger;
+import fiji.plugin.trackmate.FeatureModel;
+import fiji.plugin.trackmate.LoadTrackMatePlugIn_;
 import fiji.plugin.trackmate.Model;
-import fiji.plugin.trackmate.Settings;
 import fiji.plugin.trackmate.TrackMate;
 import fiji.plugin.trackmate.action.AbstractTMAction;
 import fiji.plugin.trackmate.action.TrackMateAction;
 import fiji.plugin.trackmate.action.TrackMateActionFactory;
+import fiji.plugin.trackmate.features.edges.EdgeTimeLocationAnalyzer;
+import fiji.plugin.trackmate.features.edges.EdgeVelocityAnalyzer;
 import fiji.plugin.trackmate.gui.TrackMateGUIController;
-import fiji.plugin.trackmate.io.TmXmlReader;
 import fiji.plugin.trackmate.util.TMUtils;
 
 public class VelocityAnalysisAction extends AbstractTMAction
@@ -91,7 +99,77 @@ public class VelocityAnalysisAction extends AbstractTMAction
 			logger.error( thresholder.getErrorMessage() );
 			return;
 		}
+
 		logger.log( "Velocity thresholding done.\n" );
+
+		/*
+		 * Declare features
+		 * 
+		 * Now, this is going to be a bit weird: This action is going to
+		 * generate new track features. They were not declared before the first
+		 * time this action is run, and they are not in sync against
+		 * modification of the model. Let's see how it rolls in the GUI.
+		 */
+
+		final FeatureModel fm = model.getFeatureModel();
+		fm.declareTrackFeatures( FEATURES, FEATURE_NAMES, FEATURE_SHOTRT_NAMES, FEATURE_DIMENSIONS, IS_INT );
+
+		/*
+		 * Assign feature values.
+		 */
+
+		final Map< Integer, List< List< DefaultWeightedEdge >>> trackGaps = thresholder.getTrackGaps();
+		final Map< Integer, List< List< DefaultWeightedEdge >>> trackRuns = thresholder.getTrackRuns();
+		final Set< Integer > trackIDs = trackRuns.keySet();
+
+
+		for ( final Integer id : trackIDs )
+		{
+			final List< List< DefaultWeightedEdge >> gaps = trackGaps.get( id );
+			final List< List< DefaultWeightedEdge >> runs = trackRuns.get( id );
+
+			// Number of pauses.
+			final int nPauses = gaps.size();
+			fm.putTrackFeature( id, NUMBER_OF_PAUSES, Double.valueOf( nPauses ) );
+
+			// Mean duration of pauses.
+			double totalPauseDuration = 0d;
+			for ( final List< DefaultWeightedEdge > gap : gaps )
+			{
+				if ( gap.size() < 1 )
+				{
+					continue;
+				}
+				final DefaultWeightedEdge firstEdge = gap.get( 0 );
+				final DefaultWeightedEdge lastEdge = gap.get( gap.size() - 1 );
+				final double tf = fm.getEdgeFeature( firstEdge, EdgeTimeLocationAnalyzer.TIME );
+				final double tl = fm.getEdgeFeature( lastEdge, EdgeTimeLocationAnalyzer.TIME );
+				totalPauseDuration += ( tl - tf );
+			}
+			final double meanPauseDuration = totalPauseDuration / nPauses;
+			fm.putTrackFeature( id, PAUSE_MEAN_DURATION, Double.valueOf( meanPauseDuration ) );
+
+			// Mean velocity without pauses.
+			double totalVelocity = 0d;
+			int nVelocity = 0;
+			for ( final List< DefaultWeightedEdge > run : runs )
+			{
+				if ( run.size() < 1 )
+				{
+					continue;
+				}
+				for ( final DefaultWeightedEdge edge : run )
+				{
+					final double v = fm.getEdgeFeature( edge, EdgeVelocityAnalyzer.VELOCITY );
+					totalVelocity += v;
+					nVelocity++;
+				}
+			}
+			final double meanVelocity = totalVelocity / nVelocity;
+			fm.putTrackFeature( id, MEAN_VELOCITY_NO_PAUSES, Double.valueOf( meanVelocity ) );
+		}
+
+		logger.log( "Additional features calculated.\n" );
 
 		logger.log( "Done.\n" );
 
@@ -136,29 +214,78 @@ public class VelocityAnalysisAction extends AbstractTMAction
 	public static void main( final String[] args )
 	{
 		final File file = new File( "e:/Users/JeanYves/Documents/Projects/Optofluidics/MovieForTradeshow/Movie.xml" );
-		final TmXmlReader reader = new TmXmlReader( file );
-		if ( !reader.isReadingOk() )
-		{
-			System.err.println( reader.getErrorMessage() );
-			// return;
-		}
+		// final TmXmlReader reader = new TmXmlReader( file );
+		// if ( !reader.isReadingOk() )
+		// {
+		// System.err.println( reader.getErrorMessage() );
+		// // return;
+		// }
+		//
+		// final Model model = reader.getModel();
+		// if ( !reader.isReadingOk() )
+		// {
+		// System.err.println( reader.getErrorMessage() );
+		// // return;
+		// }
+		//
+		// final VelocityAnalysisAction action = new VelocityAnalysisAction(
+		// null );
+		// action.setLogger( Logger.DEFAULT_LOGGER );
+		// final TrackMate trackmate = new TrackMate( model, new Settings() );
+		//
+		// action.execute( trackmate );
 
-		final Model model = reader.getModel();
-		if ( !reader.isReadingOk() )
-		{
-			System.err.println( reader.getErrorMessage() );
-			// return;
-		}
+		final LoadTrackMatePlugIn_ plugin = new LoadTrackMatePlugIn_();
+		plugin.run( file.getAbsolutePath() );
 
-		final VelocityAnalysisAction action = new VelocityAnalysisAction( null );
-		action.setLogger( Logger.DEFAULT_LOGGER );
-		final TrackMate trackmate = new TrackMate( model, new Settings() );
+	}
 
-		action.execute( trackmate );
+	/*
+	 * FEATURE DECLARATION
+	 */
 
-		// final LoadTrackMatePlugIn_ plugin = new LoadTrackMatePlugIn_();
-		// plugin.run( file.getAbsolutePath() );
+	private static final String NUMBER_OF_PAUSES = "NUMBER_OF_PAUSES";
 
+	private static final String PAUSE_MEAN_DURATION = "PAUSE_MEAN_DURATION";
+
+	private static final String MEAN_VELOCITY_NO_PAUSES = "MEAN_VELOCITY_NO_PAUSES";
+
+	private static final Collection< String > FEATURES;
+
+	private static final Map< String, String > FEATURE_NAMES;
+
+	private static final Map< String, String > FEATURE_SHOTRT_NAMES;
+
+	private static final Map< String, Dimension > FEATURE_DIMENSIONS;
+
+	private static final Map< String, Boolean > IS_INT;
+
+	static
+	{
+		FEATURES = new ArrayList< String >( 3 );
+		FEATURES.add( NUMBER_OF_PAUSES );
+		FEATURES.add( PAUSE_MEAN_DURATION );
+		FEATURES.add( MEAN_VELOCITY_NO_PAUSES );
+
+		FEATURE_NAMES = new HashMap< String, String >( 3 );
+		FEATURE_NAMES.put( NUMBER_OF_PAUSES, "Number of pauses" );
+		FEATURE_NAMES.put( PAUSE_MEAN_DURATION, "Mean pause duration" );
+		FEATURE_NAMES.put( MEAN_VELOCITY_NO_PAUSES, "Mean velocity w/o pauses" );
+
+		FEATURE_SHOTRT_NAMES = new HashMap< String, String >( 3 );
+		FEATURE_SHOTRT_NAMES.put( NUMBER_OF_PAUSES, "N pauses" );
+		FEATURE_SHOTRT_NAMES.put( PAUSE_MEAN_DURATION, "Pause duration" );
+		FEATURE_SHOTRT_NAMES.put( MEAN_VELOCITY_NO_PAUSES, "Mean V. w/o pauses" );
+
+		FEATURE_DIMENSIONS = new HashMap< String, Dimension >( 3 );
+		FEATURE_DIMENSIONS.put( NUMBER_OF_PAUSES, Dimension.NONE );
+		FEATURE_DIMENSIONS.put( PAUSE_MEAN_DURATION, Dimension.TIME );
+		FEATURE_DIMENSIONS.put( MEAN_VELOCITY_NO_PAUSES, Dimension.VELOCITY );
+
+		IS_INT = new HashMap< String, Boolean >( 3 );
+		IS_INT.put( NUMBER_OF_PAUSES, Boolean.TRUE );
+		IS_INT.put( PAUSE_MEAN_DURATION, Boolean.FALSE );
+		IS_INT.put( MEAN_VELOCITY_NO_PAUSES, Boolean.FALSE );
 	}
 
 }
