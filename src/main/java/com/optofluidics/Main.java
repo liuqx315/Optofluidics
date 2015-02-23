fd205ede93dfd98cef9922f123a338778a915409
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2015-02-23 23:35:36 +0100

    New large icon from Optofluidics public logo.
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src/main/java/com/optofluidics/Main.java
+++ src/main/java/com/optofluidics/Main.java
@@ -7,6 +7,8 @@ public class Main
 
 	public static final ImageIcon OPTOFLUIDICS_ICON = new ImageIcon( Main.class.getResource( "OptofluidicsLogo_16.png" ) );
 
+	public static final ImageIcon OPTOFLUIDICS_LARGE_ICON = new ImageIcon( Main.class.getResource( "OptofluidicsLogo_512.png" ) );
+
 	public static final ImageIcon OPTOFLUIDICS_ORANGE_LOGO = new ImageIcon( Main.class.getResource( "OptofluidicsOrangeLogo.jpg" ) );
 
 	public static final String OPTOFLUIDICS_LIB_VERSION = "v0.0.11-SNAPSHOT";
