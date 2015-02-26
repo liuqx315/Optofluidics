f2c035e00ad167459087e7c7d821e663f91fb379
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-02-26 18:03:31 +0100

    Skeleton plugin for the removal of still defects.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

---
+++ src/main/java/com/optofluidics/plugin/StillSubtractor_.java
@@ -0,0 +1,20 @@
+package com.optofluidics.plugin;
+
+import ij.plugin.PlugIn;
+
+public class StillSubtractor_ implements PlugIn
+{
+
+	public static enum Method
+	{
+		MEDIAN;
+	}
+
+	@Override
+	public void run( final String arg )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
