2b30b77ea9b5ec81ef8fe4727cc2e0b57d95027b
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-03-27 13:46:06 +0100

    Bump to next development cycle.
    v0.0.17-SNAPSHOT
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/com/optofluidics/Main.java
+++ src/main/java/com/optofluidics/Main.java
@@ -11,7 +11,7 @@ public class Main
 
 	public static final ImageIcon OPTOFLUIDICS_ORANGE_LOGO = new ImageIcon( Main.class.getResource( "OptofluidicsOrangeLogo.jpg" ) );
 
-	public static final String OPTOFLUIDICS_LIB_VERSION = "v0.0.16";
+	public static final String OPTOFLUIDICS_LIB_VERSION = "v0.0.17-SNAPSHOT";
 
 	private Main()
 	{}
